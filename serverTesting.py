21c5553918b703cae74889e856238dfaa0ffd81b
root <root@ip-172-31-60-218.ec2.internal>
2023-02-21 09:32:39 +0000

    Modified 2 time serverTesting.py

--- serverTesting.py
+++ serverTesting.py
@@ -6,8 +6,8 @@ resp =client.describe_instances()
 
 print(resp['Reservations'][0]['Instances'][0]['InstanceId'])
 
-file_to_upload = 'BackUps/' + "bucket.txt"
+file_to_upload = 'BackUps/' + "serverTesting.py"
 bucket = 'theclouduser'
 print("HY")
 client2= boto3.client('s3')
-client2.upload_file("bucket.txt", bucket, file_to_upload)
+client2.upload_file("serverTesting.py", bucket, file_to_upload)
