cfafe36776071ffad7fd960b1683785f11a2f978
root <root@ip-172-31-60-218.ec2.internal>
2023-02-21 09:29:59 +0000

    Modified 2 time serverTesting.py

--- serverTesting.py
+++ serverTesting.py
@@ -1,7 +1,13 @@
 import boto3
-
+import os
 client= boto3.client('ec2', region_name='us-east-1')
 
 resp =client.describe_instances()
 
 print(resp['Reservations'][0]['Instances'][0]['InstanceId'])
+
+file_to_upload = 'BackUps/' + "bucket.txt"
+bucket = 'theclouduser'
+print("HY")
+client2= boto3.client('s3')
+client2.upload_file("bucket.txt", bucket, file_to_upload)
